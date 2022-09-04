e969c157a96af4579b2bb5e0a80a809cc5e84c31
Ismail Omodara <ismailomodara@Ismails-MacBook-Pro-2.local>
2022-09-04 13:38:41 +0100

    Updated Logic for Auth Register, JWT and Password Hashing

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,8 +1,30 @@
+const User = require("../models/User");
+const {CustomAPIError} = require("../errors/index")
+
 const register = async (req, res) => {
-    res.send("Register user")
+
+    const user = await User.create({ ...req.body })
+    const token = user.createJWT();
+
+    res.status(200).json({
+        success: true,
+        message: "User created",
+        data: {
+            user,
+            token
+        }
+    })
 }
 
 const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    if(!username || !password) {
+        throw new CustomAPIError('Please provide email and password', 400)
+    }
+
+    const token = User.createJWT()
+
     res.send("Login")
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { register, login } = require("../controllers/Auth");
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/auth/register", register);
+router.post("/auth/login", login);
 
 module.exports = router;
